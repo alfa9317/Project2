2d1eca79d402ef6d501abcab6726ff62b083bb6d
alecvipa <hebrit_626@hotmail.com>
2019-12-19 19:23:16 -0600

    version with models added

---
+++ models/Customer.js
@@ -0,0 +1,84 @@
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define("Customer", {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      validate: {
+        len: [1]
+      }
+    },
+    Name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Exterior: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Interior: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    ZipCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Province: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    City: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    State: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    PhoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+  return Customer;
+};
